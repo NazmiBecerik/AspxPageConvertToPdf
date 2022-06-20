a0b406514567ef3f9537043fce3d1d863d83a111
Nazmi Becerik <nazmibecerik@icloud.com>
2022-06-20 19:01:33 +0300

    Aspx Page Converted To Pdf

---
+++ AspxConverter/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" EnableEventValidation="false" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AspxConverter.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+              <h2>HTML Table</h2>
+
+<table>
+  <tr>
+    <th>Kullanıcı</th>
+    <th>İletişim</th>
+    <th>Ülke</th>
+  </tr>
+  <tr>
+    <td>Nazmi Becerik</td>
+    <td>5076329771</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Ali Naki Yıldırım</td>
+    <td>5055550505</td>
+    <td>Türkiye</td>
+  </tr>
+  <tr>
+    <td>Özkan Sarıboğa</td>
+    <td>5455854545</td>
+    <td>Austria</td>
+  </tr>
+
+</table>
+            <asp:Button Text="Pdf İndir" ID="btnExport_Click" OnClick="BtnExport_Click" runat="server"/>
+        
+        </div>
+    </form>
+</body>
+</html>
---
+++ AspxConverter/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using iTextSharp.text;
+using iTextSharp.tool.xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspxConverter
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnExport_Click(object sender, EventArgs e)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter hw = new HtmlTextWriter(sw))
+                {
+                    Page.RenderControl(hw);
+                    StringReader sr = new StringReader(sw.ToString());
+                    Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 10f);
+                    // Bu satırda get instance arada kayboluyor.
+                    iTextSharp.text.pdf.PdfWriter writer = iTextSharp.text.pdf.PdfWriter.GetInstance(pdfDoc, Response.OutputStream); // PdfWriter.GetInstance(ManagementReportDoc, file);
+                    pdfDoc.Open();
+                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    pdfDoc.Close();
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("content-disposition", "attachment;filename=pdf1.pdf");
+                    Response.Write(pdfDoc);
+                    Response.End();
+
+                }
+            }
+        }
+    }
+}
